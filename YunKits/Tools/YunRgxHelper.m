7ad9390a4e76fc29978c47ee8d3f90c3e0bde18b
yun <yun@yundeiMac.local>
2017-08-03 10:58:54 +0800

    fix api

--- YunKits/Tools/YunRgxHelper.m
+++ YunKits/Tools/YunRgxHelper.m
@@ -14,19 +14,29 @@
             regularExpressionWithPattern:rgx
                                  options:0
                                    error:&error];
+
+    return error == nil;
+}
+
++ (NSString *)matchedCtnByRgx:(NSString *)rgx ctn:(NSString *)ctn {
+    NSError *error;
+    // 创建NSRegularExpression对象并指定正则表达式
+    NSRegularExpression *regex = [NSRegularExpression
+            regularExpressionWithPattern:rgx
+                                 options:0
+                                   error:&error];
+
     if (!error) { // 如果没有错误
         // 获取特特定字符串的范围
         NSTextCheckingResult *match = [regex firstMatchInString:ctn
                                                         options:0
                                                           range:NSMakeRange(0, [ctn length])];
         if (match) {
-            return YES;
+            return [ctn substringWithRange:match.range];
         }
     }
-    else {
-    }
 
-    return NO;
+    return nil;
 }
 
 @end
