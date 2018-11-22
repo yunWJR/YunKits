f3e6875402842e412a36f66b5b756b402bf903b1
yun <>
2018-11-22 16:23:47 +0800

    reform code

--- YunKits/Tools/YunTypeDefine.h
+++ YunKits/Tools/YunTypeDefine.h
@@ -39,4 +39,7 @@
 #define UI_Height_TabBar ((IS_IPHONE_X==YES || IS_IPHONE_Xr ==YES || IS_IPHONE_Xs== YES || IS_IPHONE_Xs_Max== YES) ? 83.0 : 49.0)
 
 @interface YunTypeDefine : NSObject
+
++ (BOOL)isIPhoneXAndOn;
+
 @end
--- YunKits/Tools/YunTypeDefine.m
+++ YunKits/Tools/YunTypeDefine.m
@@ -3,6 +3,7 @@
 // Copyright (c) 2018 yun. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
 #import "YunTypeDefine.h"
 
 @interface YunTypeDefine () {
@@ -11,4 +12,9 @@
 @end
 
 @implementation YunTypeDefine
+
++ (BOOL)isIPhoneXAndOn {
+    return IS_IPHONE_X || IS_IPHONE_Xr || IS_IPHONE_Xs || IS_IPHONE_Xs_Max;
+}
+
 @end
