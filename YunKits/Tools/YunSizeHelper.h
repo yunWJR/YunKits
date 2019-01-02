211621f949007b6659cf54519aaabcf333b124d0
yun <>
2019-01-02 13:33:19 +0800

    add size api

--- YunKits/Tools/YunSizeHelper.h
+++ YunKits/Tools/YunSizeHelper.h
@@ -20,10 +20,34 @@
 
 + (CGFloat)screenHeight;
 
++ (CGFloat)widthOn320:(CGFloat)width;
+
++ (CGFloat)widthOnIPhone6:(CGFloat)width;
+
++ (CGFloat)heightOnIPhone6:(CGFloat)height;
+
++ (CGFloat)widthOn414:(CGFloat)width;
+
++ (CGFloat)heightOn736:(CGFloat)height;
+
++ (CGFloat)widthOnIPhone8Plus:(CGFloat)width;
+
++ (CGFloat)heightOnIPhone8Plus:(CGFloat)height;
+
 + (CGFloat)widthOn2x:(CGFloat)width;
 
 + (CGFloat)heightOn2x:(CGFloat)height;
 
 + (CGFloat)btmSafeOff;
+
++ (CGFloat)heightOn568:(CGFloat)height;
+
++ (CGFloat)widthOnIPhone5:(CGFloat)width;
+
++ (CGFloat)heightOnIPhone5:(CGFloat)height;
+
++ (CGFloat)widthOn375:(CGFloat)width;
+
++ (CGFloat)heightOn667:(CGFloat)height;
 
 @end
--- YunKits/Tools/YunSizeHelper.m
+++ YunKits/Tools/YunSizeHelper.m
@@ -38,6 +38,54 @@
     return [UIScreen mainScreen].bounds.size.height;
 }
 
++ (CGFloat)widthOn320:(CGFloat)width {
+    return self.screenWidth * width / 320.00f;
+}
+
++ (CGFloat)heightOn568:(CGFloat)height {
+    return self.screenHeight * height / 568.00f;
+}
+
++ (CGFloat)widthOnIPhone5:(CGFloat)width {
+    return [self widthOn320:width];
+}
+
++ (CGFloat)heightOnIPhone5:(CGFloat)height {
+    return [self heightOn568:height];
+}
+
++ (CGFloat)widthOn375:(CGFloat)width {
+    return self.screenWidth * width / 375.00f;
+}
+
++ (CGFloat)heightOn667:(CGFloat)height {
+    return self.screenHeight * height / 667.00f;
+}
+
++ (CGFloat)widthOnIPhone6:(CGFloat)width {
+    return [self widthOn375:width];
+}
+
++ (CGFloat)heightOnIPhone6:(CGFloat)height {
+    return [self heightOn667:height];
+}
+
++ (CGFloat)widthOn414:(CGFloat)width {
+    return self.screenWidth * width / 414.00f;
+}
+
++ (CGFloat)heightOn736:(CGFloat)height {
+    return self.screenHeight * height / 736.00f;
+}
+
++ (CGFloat)widthOnIPhone8Plus:(CGFloat)width {
+    return [self widthOn414:width];
+}
+
++ (CGFloat)heightOnIPhone8Plus:(CGFloat)height {
+    return [self heightOn736:height];
+}
+
 + (CGFloat)widthOn2x:(CGFloat)width {
     return self.screenWidth * width / 375.00f;
 }
